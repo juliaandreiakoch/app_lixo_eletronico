498a58b479197085a2e428b0a08f6b9071b9c1c7
Júlia Koch <juliaandreiakoch@gmail.com>
2024-03-29 23:50:30 -0300

    feat: creating login page

---
+++ src/components/header.js
@@ -0,0 +1,35 @@
+import { StyleSheet, View, Text, Image } from 'react-native';
+
+export function Header() {
+  return (
+    <View style={styles.header}>
+      <Image 
+        source={require('../../assets/logoIcon.png')}
+        style={styles.image}
+      />
+      <Text style={styles.appName}>DescarTech</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    padding: 50,
+  },
+  appName: {
+    fontSize: 28,
+    fontWeight: 'normal',
+    marginLeft: 8,
+    paddingTop: 12,
+  },
+  image: {
+    width: 60, 
+    height: 60,
+    marginLeft: 0,
+    borderRadius: 30,
+  },   
+});
---
+++ src/components/login.js
@@ -0,0 +1,95 @@
+import { StyleSheet, View, KeyboardAvoidingView, TextInput, TouchableOpacity, Text } from 'react-native';
+import { Header } from './header';
+
+export function Login() {
+  return (
+    <KeyboardAvoidingView style={styles.background}>
+      <Header/>
+      <View style={styles.loginContainer}>
+        <Text style={styles.title}>Login</Text>
+        <Text style={styles.subtitle}>Sign in to continue.</Text>
+        <Text style={styles.credentials}>NAME</Text>
+        <TextInput 
+          placeholder='Name'
+          autCorrect={false}
+          onChangeText={() => {}}
+          style={styles.input}
+        />
+        <Text style={styles.credentials}>PASSWORD</Text>
+        <TextInput 
+          placeholder='Password'
+          autCorrect={false}
+          onChangeText={() => {}}
+          style={styles.input}
+        />
+        <TouchableOpacity>
+          <Text style={styles.button}>Log in</Text>
+        </TouchableOpacity>
+        <Text style={styles.forgotPassword}>Forgot Password?</Text>
+        <TouchableOpacity>
+          <Text style={styles.signupButton}>Signup!</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#D4D6C6',
+  },
+  credentials: {
+    fontSize: 11,
+    marginTop: 10,
+    marginBottom: 5,
+    alignSelf: 'flex-start',
+    marginLeft: 35,
+  },
+  loginContainer: {
+    flex: 1,
+    alignItems: 'center', 
+    justifyContent: 'center', 
+    width: '80%',
+    marginBottom: 300,
+  },
+  title: {
+    fontSize: 46,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 11,
+    marginBottom: 20,
+  },
+  input: {
+    width: '80%',
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#000',
+    marginBottom: 10,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+  },
+  button: {
+    fontSize: 16,
+    backgroundColor: '#71FE6A',
+    color: 'black',
+    fontWeight: 'bold',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    textAlign: 'center',
+    marginBottom: 10,
+    width: 250,
+    borderRadius: 5,
+    overflow: 'hidden',
+  },
+  forgotPassword: {
+    marginBottom: 10,
+  },
+  signupButton: {
+    fontWeight: 'bold',
+  },
+});
