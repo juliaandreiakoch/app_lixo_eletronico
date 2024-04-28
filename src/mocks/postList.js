63b602d1bd6c400715b558f1d9b828ca9a63e18b
Júlia Koch <juliaandreiakoch@gmail.com>
2024-04-28 01:50:04 -0300

    feat: posts list

---
+++ src/mocks/postList.js
@@ -0,0 +1,62 @@
+export const postList = [
+    {
+      user: "João Silva",
+      image: "https://img.odcdn.com.br/wp-content/uploads/2024/03/celular-a15_capa.jpg",
+      description: "Smartphone Samsung Galaxy A15",
+      category: "Smartphone"
+    },
+    {
+      user: "Maria Santos",
+      image: "https://img.freepik.com/fotos-gratis/laptop-cinza-na-mesa-de-madeira-simples_23-2148267466.jpg?t=st=1714246606~exp=1714250206~hmac=c0a50d569cde5125ac975f8aafb708af523cafaf2389766d905ea843330ea23f&w=740",
+      description: "Laptop ultrafino com processador Core i7 e SSD de 512GB, perfeito para trabalho e entretenimento",
+      category: "Laptop"
+    },
+    {
+      user: "Ana Oliveira",
+      image: "https://images.tcdn.com.br/img/img_prod/167552/tablet_samsung_galaxy_tab_s8_5g_sm_x706b_11_256gb_preto_15111_1_092ab160920485cc548c935fc68bff3d.jpeg",
+      description: "Tablet leve e portátil com tela de alta resolução e bateria de longa duração",
+      category: "Tablet"
+    },
+    {
+      user: "Pedro Souza",
+      image: "https://blog.retranjoias.com.br/wp-content/uploads/2022/02/horizontal-smartwatch.jpg",
+      description: "Smartwatch com monitor de frequência cardíaca, GPS integrado e resistência à água",
+      category: "Smartwatch"
+    },
+    {
+      user: "Carla Pereira",
+      image: "https://img.freepik.com/fotos-gratis/mao-segurando-a-camera-fotografica_23-2150630989.jpg?t=st=1714258601~exp=1714262201~hmac=c97df9cef9de621ca553c9142308ee332c53a57a11caf398afa0e3dc1319abb5&w=740",
+      description: "Câmera digital de alta qualidade com zoom óptico de 50x e gravação de vídeo em 4K",
+      category: "Câmera"
+    },
+    {
+      user: "Jiara Martins",
+      image: "https://img.freepik.com/fotos-gratis/fones-de-ouvido-sem-fio-com-iluminacao-de-estilo-neon-cyberpunk_23-2151074308.jpg?t=st=1714258646~exp=1714262246~hmac=dd93feb3386159ce6c84d3c1e9598280f669217c5689225655cd6d8187403197&w=740",
+      description: "Fones de ouvido sem fio com cancelamento de ruído ativo e bateria de longa duração",
+      category: "Fone"
+    },
+    {
+      user: "Juliana Oliveira",
+      image: "https://wx.mlcdn.com.br/ponzi/production/portaldalu/12278_02.jpg",
+      description: "videogame com gráficos de última geração e uma vasta biblioteca de jogos",
+      category: "Videogame"
+    },
+    {
+      user: "Rafael Silva",
+      image: "https://img.freepik.com/fotos-gratis/visao-do-ecra-do-monitor-do-computador_23-2150757432.jpg?t=st=1714258920~exp=1714262520~hmac=63ef5accf11ee38469f46ee626ce2c400530925fff2da2239ece931ba6a71956&w=740",
+      description: "Monitor de alta resolução com taxa de atualização de 144Hz, ideal para jogos",
+      category: "Monitor"
+    },
+    {
+      user: "Amanda Costa",
+      image: "https://images.kabum.com.br/produtos/fotos/102433/teclado-mecanico-gamer-hyperx-alloy-origins-hx-kb6rdx-br_teclado-mecanico-gamer-hyperx-alloy-origins-hx-kb6rdx-br_1569421283_g.jpg",
+      description: "Teclado mecânico RGB com switches Cherry MX e construção em alumínio",
+      category: "Teclado"
+    },
+    {
+      user: "Jiara Martins",
+      image: "https://img.freepik.com/fotos-gratis/equipamentos-de-informatica-brilhantes-iluminados-em-um-fundo-escuro-de-escritorio-gerado-por-inteligencia-artificial_24877-80953.jpg?t=st=1714259076~exp=1714262676~hmac=ab4b21e94626b22e6678d771bc08871471ddbee22d62f28f17a798da51dc91e5&w=826",
+      description: "Mouse gamer com sensor óptico de alta precisão e iluminação personalizável",
+      category: "Mouse"
+    },
+  ];
